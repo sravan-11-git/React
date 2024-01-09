3622d46e43659b17e863cb22f49a10db2e93769b
sravan-11-git <129060926+sravan-11-git@users.noreply.github.com>
2024-01-09 10:57:30 +0530

    modified:   src/components/RestroMenu.jsx

--- src/components/RestroMenu.jsx
+++ src/components/RestroMenu.jsx
@@ -79,13 +79,13 @@ const RestroMenu = () => {
   const mergedItemCards = [...itemCards1, ...itemCards2];
 
   return (
-    <div className="menu">
+    <div className="menu text-center bg-slate-200">
       <h1 className="font-bold m-1 text-2xl">{name}</h1>
       <p className="ml-7"> Price - {costForTwo/100} per two</p>
       <h2 className="font-bold m-1 text-xl">Menu</h2>
       <ul>
         {mergedItemCards.map((item, index) => (
-          <li className="ml-7 " key={item?.card?.info?.id}>
+          <li className="ml-7 m-3" key={item?.card?.info?.id}>
             {item?.card?.info?.name} - {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100} per two
           </li>
         ))}
